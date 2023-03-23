1e2158546c5809a5cda5d8320f0494f06476d393
thisiswoo <thisiswoo0594@gmail.com>
2023-03-23 23:18:22 +0900

    Initial commit

---
+++ src/test/java/springboot/springbootwebservice/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
