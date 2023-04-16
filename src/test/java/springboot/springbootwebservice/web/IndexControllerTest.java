7994fc6283b86b657c70eeb008e71c0f646c86d5
thisiswoo <thisiswoo0594@gmail.com>
2023-04-16 23:15:53 +0900

    119~134p thymeleaf를 이용한 화면 출력 및 테스트

---
+++ src/test/java/springboot/springbootwebservice/web/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package springboot.springbootwebservice.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DisplayName("메인페이지 로딩")
+    public void 메인페이지_로딩() {
+        // whene
+        String body = this.testRestTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
