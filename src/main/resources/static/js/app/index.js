03c54afe834b55ed923c1ae39d5e36efc6ab13e2
thisiswoo <thisiswoo0594@gmail.com>
2023-04-17 09:22:15 +0900

    135~145p 등록화면 추가

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,31 @@
+let main = {
+    init : function () {
+        let _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        let data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            // url: '/posts',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            location.reload();
+        }).fail(function (error) {
+            alert(error);
+        });
+    }
+};
+
+main.init();
